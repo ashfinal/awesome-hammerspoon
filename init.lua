d45ab31838c05d303ed9618213cdc776dd8c09ff
ashfinal <ashfinal@gmail.com>
2017-06-05 06:30:15 +0800

    add fn keybindings

--- init.lua
+++ init.lua
@@ -321,6 +321,36 @@ if string.len(resizeextra_center_keys[2]) > 0 then
     hs.hotkey.bind(resizeextra_center_keys[1], resizeextra_center_keys[2], "Center Window", function() resize_win('center') end)
 end
 
+-- Fn related keybindings
+local function catcher(event)
+    if event:getFlags()['fn'] and event:getCharacters() == "h" then
+        return true, {hs.eventtap.event.newKeyEvent({}, "left", true)}
+    elseif event:getFlags()['fn'] and event:getCharacters() == "l" then
+        return true, {hs.eventtap.event.newKeyEvent({}, "right", true)}
+    elseif event:getFlags()['fn'] and event:getCharacters() == "j" then
+        return true, {hs.eventtap.event.newKeyEvent({}, "down", true)}
+    elseif event:getFlags()['fn'] and event:getCharacters() == "k" then
+        return true, {hs.eventtap.event.newKeyEvent({}, "up", true)}
+    elseif event:getFlags()['fn'] and event:getCharacters() == "y" then
+        return true, {hs.eventtap.event.newScrollEvent({3,0},{},"line")}
+    elseif event:getFlags()['fn'] and event:getCharacters() == "o" then
+        return true, {hs.eventtap.event.newScrollEvent({-3,0},{},"line")}
+    elseif event:getFlags()['fn'] and event:getCharacters() == "u" then
+        return true, {hs.eventtap.event.newScrollEvent({0,-3},{},"line")}
+    elseif event:getFlags()['fn'] and event:getCharacters() == "i" then
+        return true, {hs.eventtap.event.newScrollEvent({0,3},{},"line")}
+    elseif event:getFlags()['fn'] and event:getCharacters() == "," then
+        local currentpos = hs.mouse.getRelativePosition()
+        return true, {hs.eventtap.leftClick(currentpos)}
+    elseif event:getFlags()['fn'] and event:getCharacters() == "." then
+        local currentpos = hs.mouse.getRelativePosition()
+        return true, {hs.eventtap.rightClick(currentpos)}
+    end
+    return false
+end
+
+fn_tapper = hs.eventtap.new({hs.eventtap.event.types.keyDown}, catcher):start()
+
 if not module_list then
     module_list = {
         "widgets/netspeed",
