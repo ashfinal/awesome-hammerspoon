8b4193be9a07506bc1f5178c5768acd5083df735
ashfinal <ashfinal@gmail.com>
2017-07-08 21:30:16 +0800

    fix netspeed wrong measuration

--- widgets/netspeed.lua
+++ widgets/netspeed.lua
@@ -1,38 +1,27 @@
 function data_diff()
-    local netspeed_in_str = 'netstat -ibn | grep -e ' .. netspeed_active_interface .. ' -m 1 | awk \'{print $7}\''
-    local netspeed_out_str = 'netstat -ibn | grep -e ' .. netspeed_active_interface .. ' -m 1 | awk \'{print $10}\''
-    local in_seq1 = hs.execute(netspeed_in_str)
-    local out_seq1 = hs.execute(netspeed_out_str)
-    if gainagain == nil then
-        gainagain = hs.timer.doAfter(1,function()
-            in_seq2 = hs.execute(netspeed_in_str)
-            out_seq2 = hs.execute(netspeed_out_str)
-        end)
+    local in_seq = hs.execute(netspeed_in_str)
+    local out_seq = hs.execute(netspeed_out_str)
+    local in_diff = in_seq - netspeed_in_seq
+    local out_diff = out_seq - netspeed_out_seq
+    if in_diff/1024 > 1024 then
+        kbin = string.format("%6.2f",in_diff/1024/1024) .. ' mb/s'
     else
-        gainagain:start()
+        kbin = string.format("%6.2f",in_diff/1024) .. ' kb/s'
     end
-
-    if out_seq2 ~= nil then
-        local in_diff = in_seq1 - in_seq2
-        local out_diff = out_seq1 - out_seq2
-        if in_diff/1024 > 1024 then
-            kbin = string.format("%6.2f",in_diff/1024/1024) .. ' mb/s'
-        else
-            kbin = string.format("%6.2f",in_diff/1024) .. ' kb/s'
-        end
-        if out_diff/1024 > 1024 then
-            kbout = string.format("%6.2f",out_diff/1024/1024) .. ' mb/s'
-        else
-            kbout = string.format("%6.2f",out_diff/1024) .. ' kb/s'
-        end
-        local disp_str = '⥄ '..kbout..'\n⥂ '..kbin
-        if darkmode_status then
-            styled_disp_str = hs.styledtext.new(disp_str,{font={size=9.0,color=white}})
-        else
-            styled_disp_str = hs.styledtext.new(disp_str,{font={size=9.0,color=black}})
-        end
-        netspeed_menubar:setTitle(styled_disp_str)
+    if out_diff/1024 > 1024 then
+        kbout = string.format("%6.2f",out_diff/1024/1024) .. ' mb/s'
+    else
+        kbout = string.format("%6.2f",out_diff/1024) .. ' kb/s'
     end
+    local disp_str = '⥄ '..kbout..'\n⥂ '..kbin
+    if darkmode_status then
+        styled_disp_str = hs.styledtext.new(disp_str,{font={size=9.0,color=white}})
+    else
+        styled_disp_str = hs.styledtext.new(disp_str,{font={size=9.0,color=black}})
+    end
+    netspeed_menubar:setTitle(styled_disp_str)
+    netspeed_in_seq = in_seq
+    netspeed_out_seq = out_seq
 end
 
 netspeed_active_interface = hs.network.primaryInterfaces()
@@ -41,7 +30,6 @@ if netspeed_active_interface ~= false then
     if not netspeed_menubar then
         netspeed_menubar = hs.menubar.new()
     end
-    data_diff()
     local interface_detail = hs.network.interfaceDetails(netspeed_active_interface)
     local menuitems_table = {}
     if interface_detail.AirPort then
@@ -59,6 +47,13 @@ if netspeed_active_interface ~= false then
     local macaddr = hs.execute('ifconfig '..netspeed_active_interface..' | grep ether | awk \'{print $2}\'')
     table.insert(menuitems_table, {title="MAC Addr: "..macaddr, tooltip="Copy MAC Address to clipboard", fn=function() hs.pasteboard.setContents(macaddr) end})
     netspeed_menubar:setMenu(menuitems_table)
+
+    netspeed_in_str = 'netstat -ibn | grep -e ' .. netspeed_active_interface .. ' -m 1 | awk \'{print $7}\''
+    netspeed_out_str = 'netstat -ibn | grep -e ' .. netspeed_active_interface .. ' -m 1 | awk \'{print $10}\''
+
+    netspeed_in_seq = hs.execute(netspeed_in_str)
+    netspeed_out_seq = hs.execute(netspeed_out_str)
+
     if nettimer == nil then
         nettimer = hs.timer.doEvery(1,data_diff)
     else
