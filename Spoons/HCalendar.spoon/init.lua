56f2f5191bc983eeb8db1b5c60b3041814e08f3d
ashfinal <ashfinal@gmail.com>
2017-09-01 18:24:02 +0800

    Trim HCalendar's canvas properly

--- Spoons/HCalendar.spoon/init.lua
+++ Spoons/HCalendar.spoon/init.lua
@@ -19,46 +19,48 @@ obj.hcalh = 100
 
 local function updateHcalCanvas()
     local titlestr = os.date("%B %Y")
-    obj.canvas[2].text = titlestr
+    obj.canvas[3].text = titlestr
     local currentyear = os.date("%Y")
     local currentmonth = os.date("%m")
     local currentday = os.date("%d")
     local weeknames = {"Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"}
     local firstdayofnextmonth = os.time{year=currentyear, month=currentmonth+1, day=1}
     local lastdayofcurrentmonth = os.date("*t", firstdayofnextmonth-24*60*60).day
-    for i=1,31 do
+    for i=1,lastdayofcurrentmonth do
         local weekdayofqueriedday = os.date("*t", os.time{year=currentyear, month=currentmonth, day=i}).wday
         local mappedweekdaystr = weeknames[weekdayofqueriedday]
-        obj.canvas[2+i].text = mappedweekdaystr
-        obj.canvas[64+i].text = i
+        obj.canvas[3+i].text = mappedweekdaystr
+        obj.canvas[65+i].text = i
         if mappedweekdaystr == "Sa" or mappedweekdaystr == "Su" then
-            obj.canvas[2+i].textColor = {hex="#FF7878"}
-            obj.canvas[33+i].fillColor = {hex="#FF7878"}
-            obj.canvas[64+i].textColor = {hex="#FF7878"}
+            obj.canvas[3+i].textColor = {hex="#FF7878"}
+            obj.canvas[34+i].fillColor = {hex="#FF7878"}
+            obj.canvas[65+i].textColor = {hex="#FF7878"}
+        else
+            -- Restore the default colors
+            obj.canvas[3+i].textColor = {hex="#FFFFFF"}
+            obj.canvas[34+i].fillColor = {hex="#FFFFFF", alpha=0.5}
+            obj.canvas[65+i].textColor = {hex="#FFFFFF"}
         end
         if i == math.tointeger(currentday) then
-            obj.canvas[33+i].fillColor = {hex="#00BAFF", alpha=0.8}
-            obj.canvas[96].frame.x = tostring((10+24*(i-1))/obj.hcalw)
-        end
-        -- hide extra day
-        if i > lastdayofcurrentmonth then
-            obj.canvas[2+i].textColor.alpha = 0
-            obj.canvas[33+i].fillColor.alpah = 0
-            obj.canvas[64+i].textColor.alpah = 0
+            obj.canvas[34+i].fillColor = {hex="#00BAFF", alpha=0.8}
+            obj.canvas[97].frame.x = tostring((10+24*(i-1))/obj.hcalw)
         end
     end
-    -- trim the canvas
-    obj.canvas:size({
-        w = lastdayofcurrentmonth*24+20,
-        h = 100
-    })
+    -- hide extra day
+    for i=lastdayofcurrentmonth+1, 31 do
+        obj.canvas[3+i].text = ""
+        obj.canvas[34+i].fillColor.alpha = 0
+        obj.canvas[65+i].text = ""
+    end
+    -- Adjust the size of clipmask to clip the canvas
+    obj.canvas[2].frame.w = tostring((lastdayofcurrentmonth*24+20)/obj.hcalw)
 end
 
 function obj:init()
-    local hcalbgcolor = {red=0, blue=0, green=0, alpha=0.3}
-    local hcaltitlecolor = {red=1, blue=1, green=1, alpha=0.3}
-    local todaycolor = {red=1, blue=1, green=1, alpha=0.2}
-    local midlinecolor = {red=1, blue=1, green=1, alpha=0.5}
+    local hcalbgcolor = {hex="#000000", alpha=0.3}
+    local hcaltitlecolor = {hex="#FFFFFF", alpha=0.3}
+    local todaycolor = {hex="#FFFFFF", alpha=0.2}
+    local midlinecolor = {hex="#FFFFFF", alpha=0.5}
     local cscreen = hs.screen.mainScreen()
     local cres = cscreen:fullFrame()
     obj.canvas = hs.canvas.new({
@@ -71,7 +73,15 @@ function obj:init()
     obj.canvas:behavior(hs.canvas.windowBehaviors.canJoinAllSpaces)
     obj.canvas:level(hs.canvas.windowLevels.desktopIcon)
 
+    -- Use one pseudo element to clip the canvas
     obj.canvas[1] = {
+        type = "rectangle",
+        action = "clip",
+        roundedRectRadii = {xRadius = 10, yRadius = 10},
+    }
+
+    -- *NOW* we can draw our actual "visible" parts
+    obj.canvas[2] = {
         id = "hcal_bg",
         type = "rectangle",
         action = "fill",
@@ -79,7 +89,7 @@ function obj:init()
         roundedRectRadii = {xRadius = 10, yRadius = 10},
     }
 
-    obj.canvas[2] = {
+    obj.canvas[3] = {
         id = "hcal_title",
         type = "text",
         text = "",
@@ -95,7 +105,7 @@ function obj:init()
     }
 
     -- upside weekday string
-    for i=3, 3+30 do
+    for i=4, 4+30 do
         obj.canvas[i] = {
             type = "text",
             text = "",
@@ -103,7 +113,7 @@ function obj:init()
             textSize = 13,
             textAlignment = "center",
             frame = {
-                x = tostring((10+24*(i-3))/obj.hcalw),
+                x = tostring((10+24*(i-4))/obj.hcalw),
                 y = "45%",
                 w = tostring(24/(obj.hcalw-20)),
                 h = "23%"
@@ -112,13 +122,13 @@ function obj:init()
     end
 
     -- midline rectangle
-    for i=34, 34+30 do
+    for i=35, 35+30 do
         obj.canvas[i] = {
             type = "rectangle",
             action = "fill",
             fillColor = midlinecolor,
             frame = {
-                x = tostring((10+24*(i-34))/obj.hcalw),
+                x = tostring((10+24*(i-35))/obj.hcalw),
                 y = "65%",
                 w = tostring(24/(obj.hcalw-20)),
                 h = "4%"
@@ -127,7 +137,7 @@ function obj:init()
     end
 
     -- downside day string
-    for i=65, 65+30 do
+    for i=66, 66+30 do
         obj.canvas[i] = {
             type = "text",
             text = "",
@@ -135,7 +145,7 @@ function obj:init()
             textSize = 13,
             textAlignment = "center",
             frame = {
-                x = tostring((10+24*(i-65))/obj.hcalw),
+                x = tostring((10+24*(i-66))/obj.hcalw),
                 y = "70%",
                 w = tostring(24/(obj.hcalw-20)),
                 h = "23%"
@@ -144,7 +154,7 @@ function obj:init()
     end
 
     -- today cover rectangle
-    obj.canvas[96] = {
+    obj.canvas[97] = {
         type = "rectangle",
         action = "fill",
         fillColor = todaycolor,
